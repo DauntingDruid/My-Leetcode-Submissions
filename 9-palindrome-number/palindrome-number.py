a47ebdc4df399ce4a1ca14eee4107c848a328501
Abhishek Tomar <37101503+DauntingDruid@users.noreply.github.com>
2026-01-23 21:14:55 +1100

    Time: 9 ms (62.32%) | Memory: 12.5 MB (15.61%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -9,13 +9,7 @@ class Solution(object):
         i=0
         j=len(str(x))-1
 
-        print(s)
-        print(xlen/2)
-        print(i)
-        print(j)
-
         while i <= xlen/2 and j >= xlen/2:
-            print(s[i],s[j])
             if s[i] is not s[j]:
                 return False
             else: 
