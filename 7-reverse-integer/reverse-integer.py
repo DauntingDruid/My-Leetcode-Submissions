f789a0a51bf0bf05dc8fcfba20449e9b186bc0e1
Abhishek Tomar <37101503+DauntingDruid@users.noreply.github.com>
2026-01-25 17:48:48 +1100

    Time: 18 ms (54.68%) | Memory: 12.5 MB (51.94%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -16,7 +16,7 @@ class Solution(object):
             j = 1
             flag = 1
 
-        # Reverse String
+        # Swap characters in list
         while i >= ((len(s) - 1)//2) + 1 and j <= (len(s) - 1)//2:
             temp = s[j]
             s[j] = s[i]
@@ -24,9 +24,10 @@ class Solution(object):
             i -= 1 
             j += 1
 
-        # Constraint Check
+        # Join characters in list and converting to int
         res = int("".join(s))
 
+        # Constraint check
         if -2**31 <= res and res <= (2**31 - 1) :
             return res
         else:
